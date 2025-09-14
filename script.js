f26801e2491c8c702fb69f2e83acebd114b05465
mohamed-janahi <Mohamed.janahi98@gmail.com>
2025-09-14 13:00:55 +0300

    moved the ball and obstacle using css

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const start = document.querySelector("button")
-button.addeventlistener("click")
-
 const restart = document.querySelector("button.repeat")
 button.addeventlistener("click")
+
+const obstacle = document.querySelector(".obstacle")
+const gameArea = document.querySelector(".gameArea")
+
+const ball = document.querySelector(".ball")
+
+setInterval(ball, 1000)
+console.log(setInterval(ball, 1000))
+
+setInterval(obstacle, 1000)
